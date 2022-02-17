37555fe8787e423cd103386b65cd72315dc2cce6
Wenqiang.Shu <u126869@ucstudent.win.canberra.edu.au>
2022-02-17 16:08:07 +1100

    update version

--- app/src/main/java/com/example/uieventapp/MainActivity.java
+++ app/src/main/java/com/example/uieventapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.uieventapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText = (EditText) findViewById (R.id.inputText);
+        editText.onClickListener (new View.OnClicklister(){
+
+        }
+    }
+
+    public void displayMessage (View view) {
+        final TextView textView =
+                (TextView) findViewById (R.id.textViewOutput);
+        EditText editText = (EditText) findViewById (R.id.inputText);
+        if(editText.getText().length() == 0) {
+            textView.setText("Please enter a message");
+        }else {
+            textView.setText(editText.getText());
+        }
+    }
+
+    public void empty(View view) {
+        EditText editText = (EditText) findViewById (R.id.inputText);
+        editText.setText (null);
     }
 }
